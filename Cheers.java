78ad336470af36c5185c15564d70a7a00eb5639b
Dana Gabbay <dana.gabbay@post.runi.ac.il>
2025-11-09 14:26:07 +0200

    HW2

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,27 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int n = Integer.parseInt(args[1]);
+            word = word.toUpperCase();
+            String vowels = "AEFHILMNORSX";
+            int i = 0;
+            char c;
+            while (i<word.length())
+            {
+                c = word.charAt(i);
+                if (vowels.indexOf(c) == -1) 
+                        System.out.println ("Give me a  "+ c + ": " + c + "!");
+                        else
+                        System.out.println ("Give me an "+ c + ": " + c + "!");
+                i = i+1;
+            }
+            System.out.println ("What does that spell?");
+            while (n>0) 
+            {
+            System.out.println (word+ "!!!");
+            n=n-1;
+            }
+
+            
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int time = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1) + args[0].charAt(3) + args[0].charAt(4));
+		int minutesToAdd = Integer.parseInt ("" + args [1]);
+        int hours = time/100;
+        int minutes = time%100;
+        int totalMinutes = (hours*60) + minutes + minutesToAdd;
+        int totalHours = totalMinutes/60;
+        int NewHours = totalHours%24;
+        int NewMinutes = totalMinutes - (totalHours*60);
+        if (NewHours<10)
+            System.out.print("0"+ NewHours);
+            else 
+            System.out.print (NewHours);
+        if (NewMinutes<10)
+            System.out.print(":0"+ NewMinutes);
+            else
+            System.out.print(":"+ NewMinutes);
+
+        
+
     }
 }
